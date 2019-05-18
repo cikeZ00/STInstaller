a7a12cfe489b03442088bc237d436fcad720f798
cikeZ00 <17727672+cikeZ00@users.noreply.github.com>
2019-05-18 10:40:50 -0700

    Add files via upload

---
+++ STInstaller.ps1
@@ -0,0 +1,93 @@
+function DownloadFile($url, $targetFile)
+
+{
+
+   $uri = New-Object "System.Uri" "$url"
+
+   $request = [System.Net.HttpWebRequest]::Create($uri)
+
+   $request.set_Timeout(15000) #15 second timeout
+
+   $response = $request.GetResponse()
+
+   $totalLength = [System.Math]::Floor($response.get_ContentLength()/1024)
+
+   $responseStream = $response.GetResponseStream()
+
+   $targetStream = New-Object -TypeName System.IO.FileStream -ArgumentList $targetFile, Create
+
+   $buffer = new-object byte[] 10KB
+
+   $count = $responseStream.Read($buffer,0,$buffer.length)
+
+   $downloadedBytes = $count
+
+   while ($count -gt 0)
+
+   {
+
+       $targetStream.Write($buffer, 0, $count)
+
+       $count = $responseStream.Read($buffer,0,$buffer.length)
+
+       $downloadedBytes = $downloadedBytes + $count
+
+       Write-Progress -activity "Downloading file '$($url.split('/') | Select -Last 1)'" -status "Downloaded ($([System.Math]::Floor($downloadedBytes/1024))K of $($totalLength)K): " -PercentComplete ((([System.Math]::Floor($downloadedBytes/1024)) / $totalLength)  * 100)
+
+   }
+
+   Write-Progress -activity "Finished downloading file '$($url.split('/') | Select -Last 1)'"
+
+   $targetStream.Flush()
+
+   $targetStream.Close()
+
+   $targetStream.Dispose()
+
+   $responseStream.Dispose()
+
+}
+
+Echo "
+A SkyrimTogether auto installation tool.
+
+Made by:
+          
+ :::::::: ::::::::::: :::    ::: :::::::::: ::::::::: :::::::   :::::::  
+:+:    :+:    :+:     :+:   :+:  :+:             :+: :+:   :+: :+:   :+: 
++:+           +:+     +:+  +:+   +:+            +:+  +:+  :+:+ +:+  :+:+ 
++#+           +#+     +#++:++    +#++:++#      +#+   +#+ + +:+ +#+ + +:+ 
++#+           +#+     +#+  +#+   +#+          +#+    +#+#  +#+ +#+#  +#+ 
+#+#    #+#    #+#     #+#   #+#  #+#         #+#     #+#   #+# #+#   #+# 
+ ######## ########### ###    ### ########## ######### #######   #######                                          
+
+                                                     
+"
+pause
+New-Item -Path 'C:\SkyrimTogether' -ItemType Directory
+clear
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+downloadFile "https://github.com/SkyrimTogether/issues-launcher/releases/download/v1.0.5/Harbor-1.0.5.exe" "C:\SkyrimTogether\Harbor-1.0.5.exe"
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+downloadFile "https://github.com/cikeZ00/STInstaller/raw/master/Server_r.exe" "C:\SkyrimTogether\Server_r.exe"
+
+$pathvargs = {C:\SkyrimTogether\Harbor-1.0.5.exe /S /v/qn }
+Invoke-Command -ScriptBlock $pathvargs
+
+Echo "Installation complete!"
+
+$input = read-host "Do you want to start the server? Yes or No"
+if ($input -eq 'yes'){
+    Start-Process -FilePath "C:\SkyrimTogether\Server_r.exe"
+}elseif($input -eq 'no'){
+    Echo "Alright, the server should be located in skyrim/server when you install ST"
+}else{
+    Write-Output "k"
+    }
+
+pause
+Echo "Starting Harbor"
+Start-Process -FilePath "C:\Program Files (x86)\Tilted` Phoques\Harbor\Harbor.exe"
+
